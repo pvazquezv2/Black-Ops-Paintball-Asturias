f2f32e3d3e090612fcbe2595004b8be31ea4edc0
pelayovv20 <pelayovv20@educastur.es>
2025-04-15 20:23:36 +0200

    Packages

--- src/main/java/com/pelayo/paintball/PaintballmanagerApplication.java
+++ src/main/java/com/pelayo/paintball/PaintballmanagerApplication.java
@@ -2,9 +2,16 @@ package com.pelayo.paintball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PaintballmanagerApplication {
+	
+	@Bean
+	public Principal applicationStartupRunner() {
+		return new Principal();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaintballmanagerApplication.class, args);
---
+++ src/main/java/com/pelayo/paintball/Principal.java
@@ -0,0 +1,11 @@
+package com.pelayo.paintball;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+	@Override
+	public void run (String... args) throws Exception {
+		
+	}
+
+}
