861a6f6322df5e92b1d383958b537da0913ce55c
pelayovv20 <pelayovv20@educastur.es>
2025-04-15 19:30:24 +0200

    First commit

---
+++ src/main/java/com/pelayo/paintball/PaintballmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.pelayo.paintball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintballmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintballmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelayo/paintball/PaintballmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelayo.paintball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintballmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
