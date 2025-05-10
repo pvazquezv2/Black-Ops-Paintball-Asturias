cc9188d50ebfa27b97fb8b6e17201fc30a3573da
pelayovv20 <pelayovv20@educastur.es>
2025-05-10 12:41:28 +0200

    Controllers

---
+++ src/main/java/com/pelayo/controller/EscenarioController.java
@@ -0,0 +1,54 @@
+package com.pelayo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pelayo.model.Escenario;
+import com.pelayo.service.EscenarioService;
+
+
+
+
+@Controller
+public class EscenarioController {
+	
+	@Autowired
+	private EscenarioService escenarioService;
+	
+	// Mostrar todos los escenarios
+	@GetMapping
+	public String listarEscenarios(Model model) {
+		List<Escenario> escenarios = escenarioService.verTodos();
+		model.addAttribute("escenarios", escenarios);
+		return "escenarios/lista";
+	}
+	
+	//formulario para añadir nuevo escenario
+	@GetMapping("/nuevo")
+	public String mostrarFormularioNuevo(Model model) {
+		model.addAttribute("escenario", new Escenario());
+		return "escenarios/formulario";
+	}
+	
+	//guardar escenario enviado desde el formulario
+	@PostMapping("/guardar")
+	public String guardarEscenario(@ModelAttribute Escenario escenario) {
+		escenarioService.insertar(escenario);
+		return "redirect:/escenarios";
+	}
+	
+	//eliminar escenario por ID (solo admin deberia de ver esto)
+	@GetMapping("/eliminar/{id}")
+	public String eliminarEscenario(@PathVariable("id") Long id) {
+		//escenarioService.eliminarPorId(id);
+		return "redirect:/escenarios";
+	}
+
+}
---
+++ src/main/java/com/pelayo/controller/PersonaController.java
@@ -0,0 +1,5 @@
+package com.pelayo.controller;
+
+public class PersonaController {
+
+}
