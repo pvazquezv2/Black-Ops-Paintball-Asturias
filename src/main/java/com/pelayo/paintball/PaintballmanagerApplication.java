579f239e02766cfb42f150b8d78124af9d65dc33
pelayovv20 <pelayovv20@educastur.es>
2025-06-05 19:37:45 +0200

    port on main

--- src/main/java/com/pelayo/paintball/PaintballmanagerApplication.java
+++ src/main/java/com/pelayo/paintball/PaintballmanagerApplication.java
@@ -5,13 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication(scanBasePackages = "com.pelayo")
 @EntityScan(basePackages = "com.pelayo.model")
 @EnableJpaRepositories(basePackages = "com.pelayo.repository")
 public class PaintballmanagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaintballmanagerApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(PaintballmanagerApplication.class);
 
+		// Leer el puerto desde la variable de entorno
+		Map<String, Object> props = new HashMap<>();
+		String port = System.getenv("PORT");
+		if (port != null) {
+			props.put("server.port", port);
+		}
+		app.setDefaultProperties(props);
+		app.run(args);
+	}
 }
